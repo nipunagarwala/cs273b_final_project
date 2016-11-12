7ad78eab7440f39b8ccdaa1de3981ac6d47b7cf1
yinoue93 <yinoue93@stanford.edu>
2016-11-12 14:58:37 -0800

    vector to 3D converting script

---
+++ fALFF23D.py
@@ -0,0 +1,128 @@
+from operator import truediv
+
+import numpy as np
+import csv
+import itertools
+import matplotlib as ml
+import matplotlib.pyplot as plt
+import math
+
+brainSz = (91,109,91)
+falffSz = 175493
+
+def loadfALFF(patientID):
+    """
+    Converts the fALFF brain readings to a 3D representation in numpy.
+    The coordinates should be specified in the coord.csv.
+
+    @type   patientID   :   int
+    @param  patientID   :   id of the patient to load the brain.
+
+    @rtype  threeDMat   :   3D numpy matrix
+    @return threeDMat   :   Stores information about the fALFF brain readings
+                            in 3D representation, with empty portions 0-padded
+    """
+    threeDMat = np.zeros(brainSz)
+
+    with open('coord.csv') as csvfile:
+        csvR = csv.reader(csvfile)
+        coordsList = list(csvR)
+        coords = np.array(coordsList)
+        coords = coords[1:,1:]
+        coords = coords.astype('int')-1
+
+    with open('fALFF.csv') as csvfile:
+        falffArr = next(itertools.islice(csv.reader(csvfile), patientID, None))
+
+    for (coord,falff) in zip(coords,falffArr):
+        threeDMat[coord[0],coord[1],coord[2]] = falff
+
+    return threeDMat
+
+def sizeReduction(data, targetShape, opt, poolBox=(2,2,2), filename=None):
+    """
+    Reduces the dimensionality of the 3D data stored in data to targetShape
+
+    @type   data        :   3D numpy matrix
+    @param  data        :   3D data matrix to dimension reduce from
+    @type   targetShape :   3 element int list
+    @param  targetShape :   Specifies the size of the output matrix
+    @type   opt         :   int
+    @param  opt         :   Dimension reduction methodology.
+                            opt=0: take the value in the middle
+                            opt=1: average pooling
+                            opt=2: random pooling
+                            opt=2: max pooling
+    @type   poolBox     :   3 element int list
+    @param  poolBox     :   Size of the box to "pool" from
+    @type   filename    :   String
+    @param  filename    :   Name of the file to save the reduced matrix to.
+                            The saved file can be loaded with np.load()
+
+    @rtype  threeDMatRedux:   3D numpy matrix
+    @return threeDMatRedux:   Reduced version of the input data
+    """
+    ratio = map(truediv,brainSz,targetShape)
+    threeDMatRedux = np.zeros(targetShape)
+
+    lowBoxExtension = [math.floor(i/2.0) for i in poolBox]
+    highBoxExtension = [math.ceil(i/2.0) for i in poolBox]
+
+    for x in range(targetShape[0]):
+        xDisc = math.floor(x*ratio[0])
+        for y in range(targetShape[1]):
+            yDisc = math.floor(y*ratio[1])
+            for z in range(targetShape[2]):
+                zDisc = math.floor(z*ratio[2])
+                # sample the center
+                if opt == 0:
+                    threeDMatRedux[x,y,z] = data[xDisc, yDisc, zDisc]
+                else:
+                    box = data[max(0,xDisc-lowBoxExtension[0]):min(brainSz[0],xDisc+highBoxExtension[0]),
+                                max(0,yDisc-lowBoxExtension[1]):min(brainSz[1],yDisc+highBoxExtension[1]),
+                                max(0,zDisc-lowBoxExtension[2]):min(brainSz[2],zDisc+highBoxExtension[2])]
+
+                    if not box.any():
+                        continue
+
+                    # average pooling
+                    if opt == 1:
+                        threeDMatRedux[x,y,z] = np.mean(box)
+                    # random pooling
+                    elif opt == 2:
+                        threeDMatRedux[x,y,z] = np.random.choice(np.asarray(box).reshape(-1))
+                    # max sampling
+                    else:
+                        threeDMatRedux[x,y,z] = np.max(box)
+
+    # write out to a file if filename is specified
+    if filename:
+        np.save(filename, threeDMatRedux)
+
+    return threeDMatRedux
+
+
+#a = np.load('tmp.npy')
+#plt.imshow(a[:,:,22], cmap = "gray")
+#plt.show()
+
+data = loadfALFF(4)
+
+sizeReduction(data, (45, 54, 45), opt=3, poolBox=(2,2,2), filename='tmp')
+
+plt.subplot(2,2,1)
+plt.imshow(data[:,:,44], cmap = "gray")
+
+avgPoolData = sizeReduction(data, (45, 54, 45), opt=1, poolBox=(2,2,2))
+plt.subplot(2,2,2)
+plt.imshow(avgPoolData[:,:,22], cmap = "gray")
+
+randomPollData1 = sizeReduction(data, (45, 54, 45), opt=2, poolBox=(2,2,2))
+plt.subplot(2,2,3)
+plt.imshow(randomPollData1[:,:,22], cmap = "gray")
+
+randomPollData2 = sizeReduction(data, (45, 54, 45), opt=3, poolBox=(2,2,2))
+plt.subplot(2,2,4)
+plt.imshow(randomPollData2[:,:,22], cmap = "gray")
+
+plt.show()
