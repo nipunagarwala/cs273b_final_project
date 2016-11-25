d6cecb2c76c1034663265f570947596ff3b0618b
Axel <axel@group12.54y3r0tb5ayedmdhuhnixbldqh.jx.internal.cloudapp.net>
2016-11-25 23:47:05 +0000

    Updated create_brain_binaries to contain phenotype data

--- create_brain_binaries.py
+++ create_brain_binaries.py
@@ -4,63 +4,82 @@ import json
 import csv
 
 
-#BRAIN_DIR = os.path.abspath('/data/pooledData')
 BRAIN_DIR = os.path.abspath('/data/originalfALFFData')
-PHENOTYPE_FILE = os.path.abspath('/data/processed_phenotype_data.csv')
+PHENOTYPE_FILE = os.path.abspath('/data/processed_imputed_phenotype_data.csv')
 OUTPUT_DIR = os.path.abspath('/data/binaries')
+ALL_FILES = os.path.abspath('/data/brain_binary_list.json')
 TRAIN_FILE = os.path.abspath('/data/train_brain_binary_list.json')
 TEST_FILE = os.path.abspath('/data/test_brain_binary_list.json')
 
 
-def create_feature_binary(X_feature, y_feature, filename):
-    	# Create binary format
-    	X = X_feature.flatten().tolist()
-    	y = [y_feature]
+def _create_feature_binary(X_data, X_image, y_feature, filename):
+    # Create binary format
+    X_1 = X_data.flatten().tolist()
+    X_2 = X_image.flatten().tolist()
+    y = [y_feature]
 
-    	# Label first, then 'image'
-    	out = np.array(y + X, np.float32)
+    # Label first, then 'image'
+    out = np.array(y + X_1 + X_2, np.float32)
 
-    	# Save
-    	out.tofile(filename)
+    # Save
+    out.tofile(filename)
 
 
-#def convert_brain_npy(brain_dir=BRAIN_DIR, phenotype_file=PHENOTYPE_FILE, output_dir=OUTPUT_DIR, pool_type='avgPool_', pool_size='_reduce2'):
+def _normalize_brain(brain_data):
+    # Get mean and variance
+    mean = brain_data.mean()
+    std_dev = brain_data.std()
+
+    # Normalize
+    return (brain_data - mean)/brain_data.std()
+
+
 def convert_brain_npy(brain_dir=BRAIN_DIR, phenotype_file=PHENOTYPE_FILE, output_dir=OUTPUT_DIR, prefix='original_'):
-	path_list = []	
-	with open(phenotype_file, 'r') as csvfile:
-		patient_reader = csv.reader(csvfile)
-		for patient in patient_reader:
-			patient_id = patient[0]
-			if patient_id == "":
-				continue
-			
-			patient_label = np.float32(patient[3])
-			#brain_filename = pool_type + patient_id + pool_size
-			brain_filename = prefix + patient_id
-			npy_filename = brain_filename + '.npy'
-			bin_filename = brain_filename + '.bin' 
+    path_list = []
+    with open(phenotype_file, 'r') as csvfile:
+        patient_reader = csv.reader(csvfile)
+        for patient in patient_reader:
+            patient_id = patient[0]
+            if patient_id == "":
+                continue
 
-			npy_path = os.path.join(brain_dir, npy_filename)
-			bin_path = os.path.join(output_dir, bin_filename)
-			
-			pooled_brain = np.load(npy_path)
-			pooled_brain = pooled_brain.astype(np.float32)
-			
-			create_feature_binary(pooled_brain, patient_label, bin_path)
-			path_list.append(bin_path)	
-	
-	return path_list
-	
+            # Retrieve data from phenotype CSV
+            patient_label = np.float32(patient[3])
+            phenotype_data = patient[5:16] + patient[19:]
+            phenotype_data = np.asarray(phenotype_data, dtype=np.float32)
+            # Normalize phenotype data?
+
+            # Create necessary file names
+            # brain_filename = pool_type + patient_id + pool_size
+            brain_filename = prefix + patient_id
+            npy_filename = brain_filename + '.npy'
+            bin_filename = patient_id + '.bin'
+
+            # Create necessary paths
+            npy_path = os.path.join(brain_dir, npy_filename)
+            bin_path = os.path.join(output_dir, bin_filename)
+
+            # Load brain images from .npy files
+            brain_data = np.load(npy_path)
+            brain_data = brain_data.astype(np.float32)
+            normailized_brain = _normalize_brain(brain_data)
+
+            # Create binaries from all data
+            _create_feature_binary(phenotype_data, normailized_brain, patient_label, bin_path)
+            path_list.append(bin_path)
+
+    return path_list
+
 
 def split_brain_binaries(file_list, split_fraction=0.9, train_file=TRAIN_FILE, test_file=TEST_FILE):
 	# Error handling
 	if split_fraction >= 1.0 or split_fraction <= 0.0:
 		split_fration = 0.9
-	
+
 	# Calculate number of files to split
 	num_files = len(file_list)
 	num_train = int(num_files * split_fraction)
-	
+
 	# Permute files
 	perm = np.arange(num_files)
 	np.random.shuffle(perm)
@@ -68,7 +87,7 @@ def split_brain_binaries(file_list, split_fraction=0.9, train_file=TRAIN_FILE, t
 
 	# Split file list
 	train_files = file_list[:num_train]
-	test_files = file_list[num_train:]	
+	test_files = file_list[num_train:]
 
 	with open(TRAIN_FILE, 'w') as outfile1:
 		json.dump(train_files, outfile1)
@@ -78,12 +97,10 @@ def split_brain_binaries(file_list, split_fraction=0.9, train_file=TRAIN_FILE, t
 
 
 def main():
-	file_list = convert_brain_npy()
+    file_list = convert_brain_npy()
+    with open(ALL_FILES, 'w') as outfile:
+        json.dump(file_list, outfile)
 	split_brain_binaries(file_list)
 
 if __name__ == '__main__':
 	main()
-
-
-
-
