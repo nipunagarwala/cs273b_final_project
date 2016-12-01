091787d0983358e1b1da4cdb51298ae47405e23f
Yuki <yinoue@group12.54y3r0tb5ayedmdhuhnixbldqh.jx.internal.cloudapp.net>
2016-12-01 05:54:27 +0000

    utils.py updated, now has blackOutBrain() and weights2Brain()

--- utils.py
+++ utils.py
@@ -11,8 +11,9 @@ import math
 
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
-brainSz = (91,109,91)
-falffSz = 175493
+BRAIN_SZ = (91,109,91)
+BRAIN_REGION_SZ = 116
+
 
 def loadfALFF(patientID):
     """
@@ -20,13 +21,15 @@ def loadfALFF(patientID):
     The coordinates should be specified in the coord.csv.
 
     @type   patientID   :   int
-    @param  patientID   :   id of the patient to load the brain.
+    @param  patientID   :   id of the patient to load the brain. 1 indexed!
 
     @rtype  threeDMat   :   3D numpy matrix
     @return threeDMat   :   Stores information about the fALFF brain readings
                             in 3D representation, with empty portions 0-padded
     """
-    threeDMat = np.zeros(brainSz)
+    assert not patientID==0, "Patient ID is 1 indexed!"
+
+    threeDMat = np.zeros(BRAIN_SZ)
 
     with open('/data/coord.csv') as csvfile:
         csvR = csv.reader(csvfile)
@@ -83,7 +86,7 @@ def sizeReduction(data, targetShape, opt, poolBox=(2,2,2), filename=None):
     @rtype  threeDMatRedux:   3D numpy matrix
     @return threeDMatRedux:   Reduced version of the input data
     """
-    ratio = map(truediv,brainSz,targetShape)
+    ratio = map(truediv,BRAIN_SZ,targetShape)
     threeDMatRedux = np.zeros(targetShape)
 
     lowBoxExtension = [int(math.floor(i/2.0)) for i in poolBox]
@@ -99,9 +102,9 @@ def sizeReduction(data, targetShape, opt, poolBox=(2,2,2), filename=None):
                 if opt == 0:
                     threeDMatRedux[x,y,z] = data[xDisc, yDisc, zDisc]
                 else:
-                    box = data[max(0,xDisc-lowBoxExtension[0]):min(brainSz[0],xDisc+highBoxExtension[0]),
-                                max(0,yDisc-lowBoxExtension[1]):min(brainSz[1],yDisc+highBoxExtension[1]),
-                                max(0,zDisc-lowBoxExtension[2]):min(brainSz[2],zDisc+highBoxExtension[2])]
+                    box = data[max(0,xDisc-lowBoxExtension[0]):min(BRAIN_SZ[0],xDisc+highBoxExtension[0]),
+                                max(0,yDisc-lowBoxExtension[1]):min(BRAIN_SZ[1],yDisc+highBoxExtension[1]),
+                                max(0,zDisc-lowBoxExtension[2]):min(BRAIN_SZ[2],zDisc+highBoxExtension[2])]
 
                     if not box.any():
                         continue
@@ -186,7 +189,138 @@ def loadROI_All():
         if covData is not None:
             np.save('./ROI/'+str(i), covData)
 
-def mat2visual(mat, zLocs, filename, valRange=(0,1)):
+def write2hdf5(filename, dict2store):
+    """
+    Write items in a dictionary to an hdf5file
+
+    @type   filename    :   String
+    @param  filename    :   Filename of the hdf5 file to output to.
+    @type   dict2store  :   Dict
+    @param  dict2store  :   Dictionary of items to store. The value should be an array.
+
+    """
+    with h5py.File(filename,'w') as hf:
+        for key,value in dict2store.iteritems():
+            hf.create_dataset(key, data=value,compression="lzf")
+
+def saveWeights(metafile = './chkpt/model.ckpt-99.meta', chkptfile = './chkpt/model.ckpt-99'):
+    import tensorflow as tf
+    sess = tf.Session()
+    new_saver = tf.train.import_meta_graph(metafile)
+    new_saver.restore(sess, chkptfile)
+
+    all_vars = tf.trainable_variables()
+    for v in all_vars:
+        print('-'*30)
+        print(v.name)
+        print(v.get_shape())
+        print(sess.run(v))
+
+def brainRegion2brainID(brainRegions):
+    """
+    Converts brainRegions between 1 and 116 to brainID specified in /data/region_name.csv
+    """
+    if type(brainRegions)==int:
+        brainRegions = [brainRegions]
+
+    assert not 0 in brainRegions, "Brain Region is 1 indexed!"
+
+    brainRegions.append(0)
+    brainRegions = sorted(brainRegions)
+
+    brainIDs = []
+    with open('/data/region_name.csv') as csvfile:
+        csvR = csv.reader(csvfile)
+        next(csvR)
+        for i in range(len(brainRegions)-1):
+            for j in range(brainRegions[i+1]-brainRegions[i]-1):
+                next(csvR)
+            brainIDs.append(int(next(csvR)[2]))
+
+    brainIDs = sorted(brainIDs)
+
+    return brainIDs
+
+def blackOutBrain(brainMat, brainRegions):
+    """
+    'Blacks out' the brain regions specified with 'brainRegions'
+
+    @type   brainMat    :   3D numpy matrix
+    @param  brainMat    :   describes the brain
+    @type   brainRegion :   int array
+    @param  brainRegion :   Regions of the brain regions to black out.
+                            The range should be [1,116], so 1 Indexed!
+
+    @rtype  brainMatRet :   3D numpy matrix
+    @return brainMatRet :   Describes a brain with specified regions blacked out
+    """
+    brainMatRet = np.copy(brainMat)
+
+    # find the id of the brainRegion
+    brainIDs = brainRegion2brainID(brainRegions)
+
+    # figure out which voxels blong to the brain regions
+    blackOutVoxels = [0]
+    with open('/data/region_code.csv') as csvfile:
+        csvR = csv.reader(csvfile)
+        next(csvR)
+        for row in csvR:
+            if int(row[1]) in brainIDs:
+                blackOutVoxels.append(int(row[0]))
+
+    # figure out where the blackout voxels are located
+    with open('/data/coord.csv') as csvfile:
+        csvR = csv.reader(csvfile)
+        next(csvR)
+        for i in range(len(blackOutVoxels)-1):
+            for j in range(blackOutVoxels[i+1]-blackOutVoxels[i]-1):
+                next(csvR)
+            coord = [int(i) for i in next(csvR)]
+            brainMatRet[coord[1]-1, coord[2]-1, coord[3]-1] = 0
+
+    return brainMatRet
+
+def weights2Brain(weights):
+    """
+    Constructs a 3D brain image from 'weights', which represents the value assigned for
+    each region of a brain.
+
+    @type   weights     :   3D numpy matrix
+    @param  weights     :   Values to assign for each brain region
+
+    @rtype  brainMat    :   3D numpy matrix
+    @return brainMat    :   A brain with each brain regions with the values 
+                            specified in 'weights'
+    """
+    brainMat = np.zeros(BRAIN_SZ)
+
+    id2regionDict = {}
+    with open('/data/region_name.csv') as csvfile:
+        csvR = csv.reader(csvfile)
+        next(csvR)
+        for row in csvR:
+            id2regionDict[int(row[2])] = int(row[1])-1
+
+    with open('/data/coord.csv') as csvfile:
+        csvR = csv.reader(csvfile)
+        coordsList = list(csvR)
+        coords = np.array(coordsList)
+        coords = coords[1:,1:]
+        coords = coords.astype('int')-1
+
+    with open('/data/region_code.csv') as csvfile:
+        csvR = csv.reader(csvfile)
+        next(csvR)
+
+        count = 0
+        for row in csvR:
+            coord = coords[count,:]
+            brainMat[coord[0],coord[1],coord[2]] = weights[id2regionDict[int(row[1])]]
+            count += 1
+
+    return brainMat
+
+def mat2visual(mat, zLocs, filename, valRange='auto'):
     """
     Visualizes the input numpy matrix and saves it into a file
 
@@ -204,7 +338,6 @@ def mat2visual(mat, zLocs, filename, valRange=(0,1)):
     @param  valRange    :   Specifies the maximum and minimum values of the colorbar used in imshow.
                             'auto' for auto-scaling of the input
     """
-
     _,_,c = mat.shape
     plt.close("all")
     plt.figure()
@@ -225,3 +358,18 @@ def mat2visual(mat, zLocs, filename, valRange=(0,1)):
     plt.colorbar(cax=cax)
 
     plt.savefig(filename)
+
+def coolPics():
+    # # blackout brains
+    # mat = loadfALFF(3)
+    # mat2 = blackOutBrain(mat,[1,2])
+    # mat2visual(mat, [20,40,60], 'original.png')
+    # mat2visual(mat2, [20,40,60], 'blackedOut.png')
+
+    # # brain_regions.png
+    # import random
+    # weights = [random.randint(0,500)+300 for i in range(116)]
+    # mat = weights2Brain(weights)
+    # mat2visual(mat, [20,40,60], 'brain_regions.png', [0,1000])
+    
+    pass
