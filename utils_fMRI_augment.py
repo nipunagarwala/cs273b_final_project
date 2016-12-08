ba12203d12b68028d5e414d52ffac3036eaa7bc9
yinoue93 <yinoue93@stanford.edu>
2016-12-08 09:23:31 +0000

    brain patchwork code implemented

--- utils_fMRI_augment.py
+++ utils_fMRI_augment.py
@@ -1,8 +1,10 @@
 from operator import truediv
 from utils_visual import *
 from utils import *
-import os
+from multiprocessing import Pool
 
+import os
+import random
 import numpy as np
 import csv
 import itertools
@@ -33,7 +35,6 @@ def loadfALFF(patientID):
         coords = np.array(coordsList)
         coords = coords[1:,1:]
         coords = coords.astype('int')-1
-
 
     with open('/data/processed_phenotype_data.csv') as csvfile:
         id = next(itertools.islice(csv.reader(csvfile), patientID, None))[2]
@@ -138,23 +139,139 @@ def loadfALFF_All():
         sizeReduction(data, (45, 54, 45), opt=3, poolBox=(2,2,2), filename='pooledData/maxPool_'+str(i)+'_reduce2')
         sizeReduction(data, (30, 36, 30), opt=3, poolBox=(3,3,3), filename='pooledData/maxPool_'+str(i)+'_reduce3')
 
-import re
-def augmentGeoTrans(originalDir='/data/originalfALFFData', outDir = '/data/geoTrans'):
-    for file in os.listdir(originalDir):
-        filepath = os.path.join(originalDir,file)
+def augmentGeoTrans(patientID, originalDir='/data/originalfALFFData', outDir='/data/augmented_geoTrans'):
+    filepath = os.path.join(originalDir,'original_%d.npy'%patientID)
+    brain = np.load(filepath)
+
+    for i in range(8):
+        outfilepath = os.path.join(outDir,str(patientID)+'_geoTrans_'+str(i))
+        flipped= brain.copy()
+        if i&1:
+            flipped = flipped[::-1,:,:]
+        if i&2:
+            flipped = flipped[:,::-1,:]
+        if i&4:
+            flipped = flipped[:,:,::-1]
+        np.save(outfilepath, flipped)
+
+def getGroupLabels(filename='/data/processed_phenotype_data.csv'):
+    autismIDs = []
+    controlIDs = []
+    with open(filename) as csvfile:
+        csvR = csv.reader(csvfile)
+        next(csvR)
+        for row in csvR:
+            if row[3]=='0':
+                controlIDs.append(int(row[0])) 
+            else:
+                autismIDs.append(int(row[0])) 
+    return sorted(autismIDs),sorted(controlIDs)
+
+def getBrainRegion(brainDir, patientID, brainRegions, partialBrain=None):
+    """
+
+    @type   brainRegion :   int array
+    @param  brainRegion :   Regions of the brain regions to black out.
+                            The range should be [1,116], so 1 Indexed!
+
+    @rtype  brainMatRet :   3D numpy matrix
+    @return brainMatRet :   
+    """
+    # if type(partialBrain):
+    #     partialBrain = np.zeros(BRAIN_SZ)
+    # load the base brain
+    filepath = os.path.join(brainDir,'original_%d.npy'%patientID)
+    brain = np.load(filepath)
+
+    # find the id of the brainRegion
+    brainIDs = brainRegion2brainID(brainRegions)
+
+    # figure out which voxels blong to the brain regions
+    blackOutVoxels = [0]
+    with open('/data/region_code.csv') as csvfile:
+        csvR = csv.reader(csvfile)
+        next(csvR)
+        for row in csvR:
+            if int(row[1]) in brainIDs:
+                blackOutVoxels.append(int(row[0]))
+
+    # figure out where the blackout voxels are located
+    with open('/data/coord.csv') as csvfile:
+        csvR = csv.reader(csvfile)
+        next(csvR)
+        for i in range(len(blackOutVoxels)-1):
+            for j in range(blackOutVoxels[i+1]-blackOutVoxels[i]-1):
+                next(csvR)
+            coord = [int(i) for i in next(csvR)]
+            partialBrain[coord[1]-1, coord[2]-1, coord[3]-1] = brain[coord[1]-1, coord[2]-1, coord[3]-1]
+
+    return partialBrain
+
+def augmentPatchwork(patientID=None, numStealRegions=None, autistic=None, 
+                     brainDir='/data/originalfALFFData', 
+                     outDir='/data/augmented_geoTrans'):
+    autismIDs,controlIDs = getGroupLabels()
+
+    if patientID:
+        # base the patchwork from a brain specified
+        # load the base brain
+        filepath = os.path.join(brainDir,'original_%d.npy'%patientID)
         brain = np.load(filepath)
 
-        patientID = re.search('[0-9]+',file).group(0)
+        ids = autismIDs if (patientID in autismIDs) else controlIDs
+        ids.remove(patientID)
 
-        for i in range(8):
-            outfilepath = os.path.join(outDir,str(patientID)+'_geoTrans_'+str(i))
-            flipped= brain.copy()
-            if i&1:
-                flipped = brain[::-1,::-1,:]
-            np.save(flipped, outfilepath)
+        # now steal brain regions from other brains
+        regions2steal = random.sample(list(xrange(1,BRAIN_REGION_SZ+1)),numStealRegions)
 
-augmentGeoTrans()
+        # black out the base brain
+        brain = blackOutBrain(brain, regions2steal)
+        regions2steal = regions2steal[:-1]
+    else:
+        # completely make a patchwork from a scratch
+        if autistic==None:
+            autistic = random.choice([True, False])
+        ids = autismIDs if autistic else controlIDs
+        brain = np.zeros(BRAIN_SZ)
+        regions2steal = list(xrange(1,BRAIN_REGION_SZ+1))
 
+    # add the brain regions
+    for region in regions2steal:
+        getBrainRegion(brainDir, random.choice(ids), region, partialBrain=brain)
 
-def augment():
-    pass
+    return brain
+
+def augmentPatchworkWorker(num):
+    autistic = augmentPatchwork(autistic=True)
+    control = augmentPatchwork(autistic=False)
+    np.save('/data/augmented_patched_autistic_allRandom/'+str(num)+'_autism_all_patched',autistic)
+    np.save('/data/augmented_patched_control_allRandom/'+str(num)+'_control_all_patched',control)
+
+def augmentPatchworkPartialWorker(runList):
+    num = runList[0]
+    filename = runList[1]
+    brain = augmentPatchwork(patientID=num, numStealRegions=25)
+    np.save(filename,brain)
+
+def augmentPatchworkPartial():
+    autistic,control = getGroupLabels()
+
+    runList = []
+    for i in range(5):
+        for a in autistic:
+            runList.append((a,'/data/augmented_patched_autistic_littleRandom/'
+                              +str(a)+'_autism_partially_patched_'+str(i)))
+        for c in control:
+            runList.append((c,'/data/augmented_patched_control_littleRandom/'
+                              +str(c)+'_autism_partially_patched_'+str(i)))
+
+    p = Pool(8)
+    p.map(augmentPatchworkPartialWorker,runList)
+
+#augmentPatchworkPartial()
+    
+def executeAugFunc(func, mapList):
+    p = Pool(8)
+    p.map(func, mapList)
+
+executeAugFunc(augmentPatchworkWorker,list(range(5000)))
