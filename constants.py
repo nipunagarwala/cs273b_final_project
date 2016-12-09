32f8db3df306bdac9a8a0df1aee4bb212c640ad0
dmcohn <dmcohn@stanford.edu>
2016-12-08 18:29:24 -0800

    Update constants.py

--- constants.py
+++ constants.py
@@ -146,13 +146,13 @@ CNN_BETA_2 = 0.2
 
 # Phenotype Neural Network (NN)
 # Must always end with a 1, to allow for logistic classification
-NN_HIDDEN_UNITS = [32, 16, 2] # [32, 32, 32, 32, 16, 16, 16, 8, 2]
+NN_HIDDEN_UNITS = [32, 32, 16, 16, 2] # [32, 32, 32, 32, 16, 16, 16, 8, 2]
 
 # Layer to be fed into Multi-Modal NN, 1-indexed
 # Ex.
 # NN_HIDDEN_UNITS = [16, 64, 64, 32, 1], NN_MMLAYER = 4
 # 	-> the layer with hidden nodes 32 (the 4th layer) will be fed into MMNN
-NN_MMLAYER = 3 # 4
+NN_MMLAYER = 4 # 4
 
 NN_REG_CONSTANTS_WEIGHTS =[0.6]*len(NN_HIDDEN_UNITS)
 NN_REG_CONSTANTS_BIAS = [0.9]*len(NN_HIDDEN_UNITS)
