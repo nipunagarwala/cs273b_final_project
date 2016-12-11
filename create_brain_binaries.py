ccd9a9dc2b49ed776fdf2f0de60815bca4b45996
Axel <axelsly@stanford.edu>
2016-12-11 21:57:42 +0000

    using patched brain images - getting good results - removed any distorted input

--- create_brain_binaries.py
+++ create_brain_binaries.py
@@ -2,22 +2,26 @@ import numpy as np
 import os
 import json
 import csv
+import argparse
 
 
 BRAIN_DIR = os.path.abspath('/data/originalfALFFData')
+BRAIN_DIR_AUG_ALL = os.path.abspath('/data/augmented_all_patched')
+BRAIN_DIR_AUG_PARTIAL = os.path.abspath('/data/augmented_partial_patched')
+
 # PHENOTYPE_FILE = os.path.abspath('/data/processed_imputed_phenotype_data.csv')
 PHENOTYPE_FILE = os.path.abspath('/data/normalized_imputed_phenotype_data.csv')
-# OUTPUT_DIR = os.path.abspath('/data/binaries')
+
 OUTPUT_DIR = os.path.abspath('/data/binaries_new2')
-# ALL_FILES = os.path.abspath('/data/brain_binary_list.json')
-# TRAIN_FILE = os.path.abspath('/data/train_brain_binary_list.json')
-# TEST_FILE = os.path.abspath('/data/test_brain_binary_list.json')
-# ALL_FILES = os.path.abspath('/data/all.json')
-# TRAIN_FILE = os.path.abspath('/data/train.json')
-# TEST_FILE = os.path.abspath('/data/test.json')
 ALL_FILES = os.path.abspath('/data/all2.json')
 TRAIN_FILE = os.path.abspath('/data/train2.json')
 TEST_FILE = os.path.abspath('/data/test2.json')
+
+OUTPUT_DIR_PARTIAL_RANDOM = os.path.abspath('/data/aug_partial_binaries')
+TRAIN_FILE_PARTIAL_RANDOM = os.path.abspath('/data/aug_partial_train.json')
+
+OUTPUT_DIR_ALL_RANDOM = os.path.abspath('/data/aug_all_binaries')
+TRAIN_FILE_ALL_RANDOM = os.path.abspath('/data/aug_all_train.json')
 
 
 def _create_feature_binary(X_data, X_image, y_feature, filename):
@@ -48,7 +52,6 @@ def create_compressed_binary(phenotype, image, label, output_dir, id, prefix='co
     _create_feature_binary(phenotype, image, label, bin_path)
     return bin_path
 
-
 def convert_brain_npy(brain_dir=BRAIN_DIR, phenotype_file=PHENOTYPE_FILE, output_dir=OUTPUT_DIR, prefix='original_'):
     path_list = []
     with open(phenotype_file, 'r') as csvfile:
@@ -62,7 +65,6 @@ def convert_brain_npy(brain_dir=BRAIN_DIR, phenotype_file=PHENOTYPE_FILE, output
             patient_label = np.float32(patient[3])
             phenotype_data = patient[5:16] + patient[19:]
             phenotype_data = np.asarray(phenotype_data, dtype=np.float32)
-            # Normalize phenotype data?
 
             # Create necessary file names
             # brain_filename = pool_type + patient_id + pool_size
@@ -73,6 +75,68 @@ def convert_brain_npy(brain_dir=BRAIN_DIR, phenotype_file=PHENOTYPE_FILE, output
             # Create necessary paths
             npy_path = os.path.join(brain_dir, npy_filename)
             bin_path = os.path.join(output_dir, bin_filename)
+
+            # Load brain images from .npy files
+            brain_data = np.load(npy_path)
+            brain_data = brain_data.astype(np.float32)
+            normailized_brain = _normalize_brain(brain_data)
+
+            # Create binaries from all data
+            _create_feature_binary(phenotype_data, normailized_brain, patient_label, bin_path)
+            path_list.append(bin_path)
+
+    return path_list
+
+
+def convert_random_brain_npy(brain_dir, output_dir, use_pheno, phenotype_file=PHENOTYPE_FILE):
+    path_list = []
+    aug_file_list = os.listdir(brain_dir)
+
+    if use_pheno:
+        with open(phenotype_file, 'r') as csvfile:
+            patient_reader = csv.reader(csvfile)
+            count = 0
+            for patient in patient_reader:
+                patient_id = patient[0]
+                if patient_id == "":
+                    continue
+                patched_brains = [f for f in aug_file_list if patient_id == f.split("_")[0]]
+
+                # Retrieve data from phenotype CSV
+                patient_label = np.float32(patient[3])
+                phenotype_data = patient[5:16] + patient[19:]
+                phenotype_data = np.asarray(phenotype_data, dtype=np.float32)
+
+                for brain_npy in patched_brains:
+                    brain_file_components = brain_npy.split(".")
+                    brain_bin = brain_file_components[0] + ".bin"
+
+                    # Create necessary paths
+                    npy_path = os.path.join(brain_dir, brain_npy)
+                    bin_path = os.path.join(output_dir, brain_bin)
+
+                    # Load brain images from .npy files
+                    brain_data = np.load(npy_path)
+                    brain_data = brain_data.astype(np.float32)
+                    normailized_brain = _normalize_brain(brain_data)
+
+                    # Create binaries from all data
+                    _create_feature_binary(phenotype_data, normailized_brain, patient_label, bin_path)
+                    path_list.append(bin_path)
+
+    else:
+        # Iterate though all augmented training files
+        for brain_npy in aug_file_list:
+            patient_label = 1.0 if "autism" in brain_npy else 0.0
+            patient_label = np.float32(patient_label)
+            phenotype_data = np.zeros(29)
+
+            file_components = brain_npy.split(".")
+            brain_bin = file_components[0] + ".bin"
+
+            # Create necessary paths
+            npy_path = os.path.join(brain_dir, brain_npy)
+            bin_path = os.path.join(output_dir, brain_bin)
 
             # Load brain images from .npy files
             brain_data = np.load(npy_path)
@@ -118,8 +182,26 @@ def save_and_split(file_list, all_files=ALL_FILES, train_file=TRAIN_FILE, test_f
 
 
 def main():
-    file_list = convert_brain_npy()
-    save_and_split(file_list)
+    parser = argparse.ArgumentParser(description='Evaluation procedure for Salami CNN.')
+    data_group = parser.add_mutually_exclusive_group()
+    random_group = parser.add_mutually_exclusive_group()
+    data_group.add_argument('--aug', action="store_true", help='Choose dataset to create')
+    random_group.add_argument('--all_random', action="store_true", help='Training the model')
+    args = parser.parse_args()
+
+    if args.aug:
+        use_pheno = not args.all_random
+        brain_dir = BRAIN_DIR_AUG_ALL if args.all_random else BRAIN_DIR_AUG_PARTIAL
+
+        output_dir = OUTPUT_DIR_ALL_RANDOM if args.all_random else OUTPUT_DIR_PARTIAL_RANDOM
+        train_file = TRAIN_FILE_ALL_RANDOM if args.all_random else TRAIN_FILE_PARTIAL_RANDOM
+
+        file_list = convert_random_brain_npy(brain_dir, output_dir, use_pheno)
+        with open(train_file, 'w') as outfile:
+            json.dump(file_list, outfile)
+    else:
+        file_list = convert_brain_npy()
+        save_and_split(file_list)
 
 if __name__ == '__main__':
 	main()
